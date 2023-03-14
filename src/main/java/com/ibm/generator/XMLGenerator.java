4c9af30c8661f9fa462c1b9366ba6fbbbe1674ac
Nigel T. Crowther <ncrowther@uk.ibm.com>
2023-03-14 15:39:38 +0000

    Add all four labs

--- src/main/java/com/ibm/generator/XMLGenerator.java
+++ src/main/java/com/ibm/generator/XMLGenerator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package generator;
+package com.ibm.generator;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
